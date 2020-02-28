a8eb27e13d56f5a2a4497b38e2a6f748996a7c28
wuduoduo <wuduoduo_2023@163.com>
2020-02-29 00:34:01 +0800

    first commit

---
+++ scripts/test.py
@@ -0,0 +1,18 @@
+import allure
+import pytest
+class Test:
+
+    @allure.severity(allure.severity_level.TRIVIAL)
+    @allure.step(title='输出第一个测试用例')
+    def test01(self):
+        allure.attach('输出','1',allure.attachment_type.PNG)
+        print('1')
+        assert 1
+
+    def test02(self):
+        print('2')
+        assert 1
+
+    def test03(self):
+        print('3')
+        assert 0
